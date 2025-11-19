678c4609ebeb1e8825761d5077bdb742293dfc38
Steve Phelps <79sphelps@gmail.com>
2025-11-19 15:47:51 -0800

    Initial commit of both client and server files

---
+++ client/src/App.js
@@ -0,0 +1,45 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
+import { HttpLink } from "@apollo/client"; // @apollo/client/link/http
+import Header from "./components/Header";
+// import Clients from "./components/Clients";
+// import AddClient from "./components/AddClient";
+// import Projects from "./components/Projects";
+import Project from "./components/Project";
+import Home from "./components/Home";
+import NotFound from "./components/NotFound";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:5000/graphql",
+});
+
+const client = new ApolloClient({
+  // uri: "http://localhost:5000/graphql",
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <>
+      <ApolloProvider client={client}>
+        <Router>
+          <Header />
+          <div className="container">
+            {/* <Projects />
+          <AddClient />
+          <Clients /> */}
+
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/projects/:id" element={<Project />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </Router>
+      </ApolloProvider>
+    </>
+  );
+}
+export default App;
---
+++ server/dummyData.js
@@ -0,0 +1,51 @@
+//Projects
+const projects = [
+  {
+    id: "1",
+    clientId: "1",
+    name: "ReactJS Website",
+    description: `Lorem ipsum dolor sit amet, consectetuer
+        adipiscing elit. Aenean commodo ligula eget
+        dolor. Aenean massa. Cum sociis natoque penatibus
+        et magnis dis parturient montes, nascetur
+        ridiculus mus. Donec quam felis, ultricies nec,
+        pellentesque eu.`,
+    status: "In Progress",
+  },
+  {
+    id: "2",
+    clientId: "2",
+    name: "ReactJS Website 2",
+    description: `Lorem ipsum dolor sit amet, consectetuer
+        adipiscing elit. Aenean commodo ligula eget
+        dolor. Aenean massa. Cum sociis natoque penatibus
+        et magnis dis parturient montes, nascetur
+        ridiculus mus. Donec quam felis, ultricies nec,
+        pellentesque eu.`,
+    status: "In Progress",
+  },
+];
+
+// Clients
+const clients = [
+  {
+    id: "1",
+    name: "Fyu Lee",
+    email: "fyulee@gmail.com",
+    phone: "343-567-4333",
+  },
+  {
+    id: "2",
+    name: "Fyu Lee 2",
+    email: "fyulee2@gmail.com",
+    phone: "343-567-4333",
+  },
+  {
+    id: "3",
+    name: "Fyu Lee 3",
+    email: "fyulee3@gmail.com",
+    phone: "343-567-4333",
+  },
+];
+
+module.exports = { projects, clients };
